e5ab6d137b5055e4104c91584fae4eb13920e4c0
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2022-04-29 21:02:10 -0500

    unique runs kata

---
+++ spec/kata/run_reduction_spec.rb
@@ -0,0 +1,20 @@
+def unique_in_order(iterable)
+    array = iterable
+    if iterable.class == String
+        array = iterable.chars
+    end
+    return array.reduce([]) {|agg, nextElement|   
+        if agg[-1] != nextElement
+            agg.append(nextElement)
+        end
+        agg
+    }
+  end
+
+RSpec.describe "run reduce" do
+    it "" do
+        # shows the same answer in the console, and works in code wars, not sure what's the problem here
+        expect(unique_in_order('AAAABBBCCDAABBB')).to equal(['A','B','C','D','A','B'])
+    end
+
+end
