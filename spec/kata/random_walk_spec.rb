b0145df909acc1bd6ddf564a4dd9403e0c6836d3
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2022-04-29 20:36:08 -0500

    random walk kata

---
+++ spec/kata/random_walk_spec.rb
@@ -0,0 +1,35 @@
+
+Point = Struct.new(:x, :y)
+def add_direction_unit(coordinate, direction)
+    if (direction == "n") 
+        coordinate.y += 1 
+    elsif direction == "s"
+        coordinate.y -= 1 
+    elsif direction == "e"
+        coordinate.x += 1
+    elsif direction == "w"
+        coordinate.x -= 1
+    end
+    return coordinate
+end
+
+def is_valid_walk(walk)
+    #your code here
+    if walk.length() != 10
+        return false
+    else
+        finalPoint = walk.reduce(Point.new(0, 0)) {|agg, nextDir| add_direction_unit(agg, nextDir) }
+        return Point.new(0, 0) == finalPoint
+    end
+  end
+
+
+RSpec.describe "String ends with" do
+    it "should pass sample tests" do
+        expect(is_valid_walk(['n','s','n','s','n','s','n','s','n','s'])).to be true;
+        expect(is_valid_walk(['w','e','w','e','w','e','w','e','w','e','w','e'])).to be false;
+        expect(is_valid_walk(['w'])).to be false;
+        expect(is_valid_walk(['n','n','n','s','n','s','n','s','n','s'])).to be false;
+    end
+end
+  
